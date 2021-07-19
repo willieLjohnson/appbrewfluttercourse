b8df18397ce0d78308bae05a02027065e9f6cf8e
SlickJohnson <liwa.johnson@gmail.com>
2021-07-19 11:15:26 -0400

    Add image

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("EEH"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("I AM RICH"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://i2.wp.com/nairametrics.com/wp-content/uploads/2020/07/gold-2.jpg'),
+          ),
+        ),
       ),
     ),
   );

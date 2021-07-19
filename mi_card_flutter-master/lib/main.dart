1d9f942477e759ad7003c558f8b52bb1166ba7e9
SlickJohnson <liwa.johnson@gmail.com>
2021-07-19 19:06:52 -0400

    Add contact info with custom font

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -14,8 +14,74 @@ class MyApp extends StatelessWidget {
           child: Column(
             children: <Widget>[
               CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage("images/profile.png"),
+                radius: 100.0,
+                backgroundImage: AssetImage('images/profile.png'),
+              ),
+              Text(
+                'Willie Johnson',
+                style: TextStyle(
+                  fontFamily: 'MajorMonoDisplay',
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Text(
+                'FULL-STACK SOFTWARE DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 15,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      size: 25.0,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text(
+                      '+1 240 907 8829',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "SourceSansPro",
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      size: 25.0,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text(
+                      'liwa.johnson@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "SourceSansPro",
+                        fontSize: 27.0,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),

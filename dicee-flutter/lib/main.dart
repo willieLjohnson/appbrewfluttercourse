ec82020a6c57f82391bb58dab7d6202f094a2836
SlickJohnson <liwa.johnson@gmail.com>
2021-07-21 16:42:06 -0400

    Make dicee buttons

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -22,14 +22,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print("Left");
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print("Right");
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
